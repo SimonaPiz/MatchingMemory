95864ad76eeab730df2e3e922d82b32f0c269f8d
Simona Pizio <simona.pizio@gmail.com>
2022-04-04 15:06:16 +0200

    Update index files

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { store } from './app/store.js';
+// Add import statement below
+import { Provider } from 'react-redux';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    // Implement Provider component with store below
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
